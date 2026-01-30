82f9643cc9b04d55d19daaa261b23d252fe94410
guibis <guilhermeplima5@gmail.com>
2026-01-30 09:39:54 -0300

    feat: Implement request logging middleware and integrate application routes.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const app = express();
+const logger = require('./middleware/logger');
+const routes = require('./routes/routes');
 
-
+app.use(logger);
 app.use(express.json());
+app.use('/', routes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
