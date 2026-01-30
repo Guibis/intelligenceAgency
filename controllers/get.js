6294cb198441d24a1a23e94d2d7f960db14df0bb
guibis <guilhermeplima5@gmail.com>
2026-01-30 08:50:24 -0300

    feat: introduce agent API endpoints for fetching and creating agents.

--- controllers/get.js
+++ controllers/get.js
@@ -2,7 +2,7 @@ const pool = require("../db");
 
 const getAgents = async (req, res) => {
     try {
-        const result = await pool.query("SELECT * FROM agents ORDER BY id ASC");
+        const result = await pool.query("SELECT * FROM agents ORDER BY id ASC;");
         res.json(result.rows);
     } catch (error) {
         console.error(error);
@@ -16,11 +16,11 @@ const getAgentById = async (req, res) => {
         if(!id) {
             return res.status(404).json({ message: "Agent not found" });
         }
-        const result = await pool.query("SELECT * FROM agents WHERE id = $1", [id]);
+        const result = await pool.query("SELECT * FROM agents WHERE id = $1;", [id]);
         res.json(result.rows);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Error fetching agent" });
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
